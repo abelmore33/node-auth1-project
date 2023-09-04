e7498a8bee59ddd5971f6cc9d7822f9f9e3a2fc0
Aaron Belmore <belmoreaaron9@gmail.com>
2023-09-03 22:15:53 -0400

    created user router

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -17,7 +17,7 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  return db("users").where("user_id", user);
+  return db("users").where("user_id", user_id);
 }
 
 /**
